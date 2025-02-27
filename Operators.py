a339a5571efc05c509cec53467c31ebba7de7d2b
Moralens Jean-Louis <woodleyj49@gmail.com>
2025-02-27 17:54:27 +0000

    Operadores en Python

---
+++ Operators.py
@@ -0,0 +1,17 @@
+# Operadores aritmeticos
+print("Suma:", 4 + 3) 
+print("Resta:", 4 - 3) 
+print("Multiplicacion:", 4 * 3) 
+print("Divicion:", 4 / 3) 
+print("Division exacta", 4 // 3)
+print("Exponente", 4 ** 3)
+print("Recordatorio", 10 % 2)
+
+# Operadores de comparacion 
+print(4 == 4) # Igual a
+print(4 == 3)
+print(4 != 3) # distinto a
+print(5 <= 3) # menor o igual a 
+print(5 >= 4) # mayor o igual
+print(6 > 7) # mayor
+print(9 < 8) # menor
