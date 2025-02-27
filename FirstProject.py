462afb430e9beea36adbfdbe64242042dc05e023
Moralens Jean-Louis <woodleyj49@gmail.com>
2025-02-27 17:52:26 +0000

    Primero programa con python

---
+++ FirstProject.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+
+lista = [1, 2, 3, 4, 5]
+for i in lista:
+    print(i)
+    if i == 4:
+        break # La instruccion break rompe cuando el bucle for se para en el numero 4
+print("Fin del bucle for")
