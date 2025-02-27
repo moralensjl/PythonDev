26c0a36133cd72cfb19b5a6e70269ff7e90d1396
Moralens Jean-Louis <woodleyj49@gmail.com>
2025-02-27 17:53:39 +0000

    Palabras reservadas

---
+++ Keywords.py
@@ -0,0 +1,10 @@
+import keyword
+print("The set of keywords in this version is: ")
+print(keyword.kwlist)
+
+# The set of keywords in this version is:
+
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await',
+# 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except',
+# 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda',
+# 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
