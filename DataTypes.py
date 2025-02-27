eb4e9e45308230c6e8ea9440b17a800b6fbe2faa
Moralens Jean-Louis <woodleyj49@gmail.com>
2025-02-27 17:51:27 +0000

    Tipos de datos en Python

---
+++ DataTypes.py
@@ -0,0 +1,18 @@
+""" Numeros en Python"""
+a = 5
+print(type(a)) # <class 'int'>
+
+b = 4.5
+print(type(b)) # <class 'float'>
+
+c = 1 + 3j # complex number
+print(type(c)) # <class 'complex'>
+print("El numero c es un numero complejo", isinstance(1 + 3j, complex)) # El numero c es un numero complejo True
+
+# Boolean
+print(True)
+print(False)
+
+# String
+funcion = "Ministro"
+print(type(funcion))
